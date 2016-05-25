ebc38dc3cbd833cc7e306e915331d7e02483edf2
anisku11 <aan.boyz10@gmail.com>
2016-05-25 20:02:41 +0700

    setting excel

--- config/excel.php
+++ config/excel.php
@@ -58,15 +58,15 @@ return array(
     ),
 
     'properties' => array(
-        'creator'        => 'Maatwebsite',
-        'lastModifiedBy' => 'Maatwebsite',
+        'creator'        => 'Lentera',
+        'lastModifiedBy' => 'Lentera',
         'title'          => 'Spreadsheet',
         'description'    => 'Default spreadsheet export',
         'subject'        => 'Spreadsheet export',
         'keywords'       => 'maatwebsite, excel, export',
         'category'       => 'Excel',
-        'manager'        => 'Maatwebsite',
-        'company'        => 'Maatwebsite',
+        'manager'        => 'Lentera',
+        'company'        => 'Lentera',
     ),
 
     /*
@@ -106,7 +106,7 @@ return array(
     |
     */
 
-    'creator'    => 'Maatwebsite',
+    'creator'    => 'Lentera',
 
     'csv'        => array(
         /*
